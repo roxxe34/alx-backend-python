aece678afbfa0842eaae42b58655c33548fe6625
roxxe34 <hamzafarissi7@gmail.com>
2024-07-04 11:19:24 +0100

    Add concat function with Python variable annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""
+concat function that takes two strings and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Concatenates two strings and returns the result.
+    """
+    return str1 + str2
