7d81c11e9270ce2fa8956df4ed37f9fc121fc517
roxxe34 <hamzafarissi7@gmail.com>
2024-07-04 13:13:02 +0100

    feat: Update variable annotations and fix function parameter in Python files
    The code changes include updating variable annotations in `4-define_variables.py` and `6-sum_mixed_list.py`. In `4-define_variables.py`, a docstring is added to clarify the purpose of the file. In `6-sum_mixed_list.py`, the function parameter is fixed to correctly indicate that the list can contain both integers and floating-point numbers.
    
    Based on the recent user commits and repository commits, it seems that there is an ongoing effort to add Python variable annotations and functions to the repository.

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+task 4
+"""
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
-    '''Computes the sum of a list of floating-point numbers.
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Computes the sum of a list of integers and floating numbers.
     '''
     return float(sum(mxd_lst))
