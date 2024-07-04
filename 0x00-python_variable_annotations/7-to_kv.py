1e1c0f4ac218210bbc976c8023af412a0c94e786
roxxe34 <hamzafarissi7@gmail.com>
2024-07-04 13:00:57 +0100

    chore: Add Python variable annotations and functions

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+'''Task 7's module.
+'''
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''returns a tuple.
+    '''
+    return (k, float(v ** 2))
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+'''Task 8's module.
+'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''Returns a function that multiplies a float by the given multiplier.
+    '''
+    return lambda x: x * multiplier
