d91c3a640f6553e92ef21d550e0087eed0c893d0
Hamza Farissi <hamzafarissi7@gmail.com>
2024-07-07 22:13:10 +0100

    done

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+"""Task 1's module.
+"""
 from typing import List
 from asyncio import gather
 
@@ -5,6 +8,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    wait-n function
+    """
     tasks = [wait_random(max_delay) for _ in range(n)]
     results = await gather(*tasks)
     return sorted(results)
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,6 +7,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    """measure_time function
+    """
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
