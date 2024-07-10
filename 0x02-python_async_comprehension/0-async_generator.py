4f92931844987f5fdf0174eca964d0d7f4826a47
Hamza Farissi <hamzafarissi7@gmail.com>
2024-07-10 01:14:45 +0100

    feat: Update async_generator module for Python async comprehension
    The code changes in this commit update the `async_generator` module for Python async comprehension. The modifications include improving the code structure and adding comments for better readability.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 '''Task 0's module.
 '''
+
+
 import random
 import asyncio
 from typing import Generator
+
 
 async def async_generator() -> Generator[float, None, None]:
     '''Generates a sequence of 10 numbers.
